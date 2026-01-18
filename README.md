a09e3d935ea9267e1d917dddbb94805b1edb749e
Manthan Thakkar <manthan@tuta.io>
2026-01-18 10:35:18 +0000

    Test camera capture.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 TODO:
 - consider max and min angles for servos and torque limits
 - spawn threads in main file and dispatch angles
-- path and arm tests
+- path, camera and arm tests
 - joint loading from files
-- Collision detection and auto checkpoints - camera integration
+- Collision detection
+- Camera integration (calibration, checkpoint generation, etc.)
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <thread>
+#include <opencv2/opencv.hpp>
+
 import quaternion;
 import arm;
 
@@ -8,6 +11,46 @@ using namespace quaternion;
 #include "../lib/doctest.h"
 #else
 int main() {
+    cv::VideoCapture cap(0);
 
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    cv::Mat img;
+    cap >> img;
+
+    cv::Mat grey;
+    cv::cvtColor(img, grey, cv::COLOR_BGR2GRAY);
+
+    cv::Mat thresh;
+    cv::threshold(grey, thresh, 128, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::findContours(
+        thresh,
+        contours,
+        hierarchy,
+        cv::RETR_TREE,
+        cv::CHAIN_APPROX_SIMPLE
+    );
+
+    std::cout << "Found " << contours.size() << " contours." << std::endl;
+
+    std::vector<std::vector<cv::Point>> filteredContours;
+    for (const std::vector<cv::Point>& c : contours) {
+        double area = cv::contourArea(c);
+        if (area > 100) { // only keep contours larger than 100 pixels
+            filteredContours.push_back(c);
+        }
+    }
+
+    std::cout << "Filtered to " << filteredContours.size() << " contours." << std::endl;
+
+    cv::Mat output = img.clone();
+    cv::drawContours(output, filteredContours, -1, cv::Scalar(0, 255, 0), 2);
+
+    cv::imwrite("image.jpg", output);
+
+    return 0;
 }
 #endif
